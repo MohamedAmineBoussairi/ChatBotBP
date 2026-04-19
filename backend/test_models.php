f378d72850022b883310d0d104846ec5a521039a
MohamedAmineBoussairi <mrmohamed2mine@gmail.com>
2026-04-19 15:20:25 +0100

    Remove test files containing leaked API keys

--- backend/test_models.php
+++
@@ -1,7 +0,0 @@
-<?php
-$key = 'AIzaSyC2VD2idwJh_RhEe84ZFb_IgH37RmK8OJQ';
-$ch = curl_init('https://generativelanguage.googleapis.com/v1beta/models?key=' . $key);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result;
